84597e6fddc69c70e09de56fdda22b3eda00d78d
KarenKrill <vampart1997@outlook.com>
2024-07-26 10:36:41 +0500

    Added project files

---
+++ Assets/Scripts/LauncherStartup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherStartup : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+}
