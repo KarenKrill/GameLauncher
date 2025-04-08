72ec67b443b4a7bda68f7a25ff7e89d1fa9a2f12
KarenKrill <vampart1997@gmail.com>
2025-04-08 20:13:20 +0500

    Ignore upload scripts

--- Assets/Common/Scripts/Utils/UpdateGameLauncherServerData.Dev.bat
+++
@@ -1,2 +0,0 @@
-scp -r -P 666 D:\Art\Programs\Unity\GameLauncher\ServerData.Dev\StandaloneWindows64 root@104.249.40.128:/var/www/html/UnityBundles.Dev/GameLauncher/
-pause
--- Assets/Common/Scripts/Utils/UpdateGameLauncherServerData.bat
+++
@@ -1,2 +0,0 @@
-scp -r -P 666 D:\Art\Programs\Unity\GameLauncher\ServerData\StandaloneWindows64 root@104.249.40.128:/var/www/html/UnityBundles/GameLauncher/
-pause
